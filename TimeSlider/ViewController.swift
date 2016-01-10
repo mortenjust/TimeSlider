79cf94bab7d1e102cc938ce0166a45d7e56ae277
mortenjust <morten.just@genstart.dk>
2016-01-10 01:06:28 -0800

    Initial Commit

---
+++ TimeSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeSlider
+//
+//  Created by Morten Just Petersen on 1/10/16.
+//  Copyright © 2016 Morten Just Petersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
